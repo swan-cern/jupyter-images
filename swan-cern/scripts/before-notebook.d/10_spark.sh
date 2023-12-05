8e70d673c0b4a2492f408d21e0ba5896ebeb7c54
Pedro Maximino <53193337+PMax5@users.noreply.github.com>
2023-12-05 15:29:15 +0100

    swan-cern: Do not exit bash script if env vars are not defined
    This was changed because the bash scripts are sourced and so, when they
    would exit, that would cause the main start.sh script to exit as well,
    leading to halting the user container.
    
    Furthermore, changed the user group of the user of the container, as it
    is defined by NB_GID environment variable.

--- swan-cern/scripts/before-notebook.d/10_spark.sh
+++ swan-cern/scripts/before-notebook.d/10_spark.sh
@@ -4,8 +4,44 @@
 # Copyright CERN
 # The environment for Spark is configured here.
 
-if [[ ! -n $SPARK_CLUSTER_NAME ]]
+if [[ -n $SPARK_CLUSTER_NAME ]]
 then
+  # Jupyter server configuration path
+  JPY_CONFIG=$JUPYTER_CONFIG_DIR/jupyter_server_config.py
+  LOCAL_IP=`hostname -i`
+  echo "$LOCAL_IP $SERVER_HOSTNAME" >> /etc/hosts
+
+  # Enable the extensions in Jupyter global path to avoid having to maintain this information 
+  # in the user scratch json file (specially because now we persist this file in the user directory and
+  # we don't want to persist the Spark extensions across sessions)
+  mkdir -p /etc/jupyter/nbconfig
+  _log "Globally enabling the Spark extensions"
+  echo "{
+    \"load_extensions\": {
+      \"sparkconnector/extension\": true,
+      \"hdfsbrowser/extension\": true
+    }
+  }" > /etc/jupyter/nbconfig/notebook.json
+  echo "{
+    \"NotebookApp\": {
+      \"jpserver_extensions\": {
+        \"hdfsbrowser.serverextension\": true
+      }
+    }
+  }" > /etc/jupyter/jupyter_notebook_config.json
+  if [ $SPARK_CLUSTER_NAME = "k8s" ]
+  then
+    NAMESPACE="analytix"
+    CLUSTER_NAME="analytix"
+  else
+    NAMESPACE=$(cat /cvmfs/sft.cern.ch/lcg/etc/hadoop-confext/conf/etc/$SPARK_CLUSTER_NAME/$SPARK_CLUSTER_NAME.info.json | python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["namespace"])')
+    CLUSTER_NAME=$SPARK_CLUSTER_NAME
+  fi
+
+  echo 'c.HDFSBrowserConfig.hdfs_site_path = "/cvmfs/sft.cern.ch/lcg/etc/hadoop-confext/conf/etc/'"$CLUSTER_NAME"'/hadoop.'"$CLUSTER_NAME"'/hdfs-site.xml"
+  c.HDFSBrowserConfig.hdfs_site_namenodes_property = "dfs.ha.namenodes.'"$NAMESPACE"'"
+  c.HDFSBrowserConfig.hdfs_site_namenodes_port = "50070"' >> $JPY_CONFIG
+else
   # Disable spark jupyterlab extensions enabled by default if no cluster is selected
   mkdir -p /etc/jupyter/labconfig
   echo "{
@@ -15,42 +51,4 @@ then
       \"jupyterlab_sparkmonitor\": true
     }
   }" > /etc/jupyter/labconfig/page_config.json
-
-  exit 0;
 fi
-
-# Jupyter server configuration path
-JPY_CONFIG=$JUPYTER_CONFIG_DIR/jupyter_server_config.py
-LOCAL_IP=`hostname -i`
-echo "$LOCAL_IP $SERVER_HOSTNAME" >> /etc/hosts
-
-# Enable the extensions in Jupyter global path to avoid having to maintain this information 
-# in the user scratch json file (specially because now we persist this file in the user directory and
-# we don't want to persist the Spark extensions across sessions)
-mkdir -p /etc/jupyter/nbconfig
-_log "Globally enabling the Spark extensions"
-echo "{
-  \"load_extensions\": {
-    \"sparkconnector/extension\": true,
-    \"hdfsbrowser/extension\": true
-  }
-}" > /etc/jupyter/nbconfig/notebook.json
-echo "{
-  \"NotebookApp\": {
-    \"jpserver_extensions\": {
-      \"hdfsbrowser.serverextension\": true
-    }
-  }
-}" > /etc/jupyter/jupyter_notebook_config.json
-if [ $SPARK_CLUSTER_NAME = "k8s" ]
-then
-  NAMESPACE="analytix"
-  CLUSTER_NAME="analytix"
-else
-  NAMESPACE=$(cat /cvmfs/sft.cern.ch/lcg/etc/hadoop-confext/conf/etc/$SPARK_CLUSTER_NAME/$SPARK_CLUSTER_NAME.info.json | python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["namespace"])')
-  CLUSTER_NAME=$SPARK_CLUSTER_NAME
-fi
-
-echo 'c.HDFSBrowserConfig.hdfs_site_path = "/cvmfs/sft.cern.ch/lcg/etc/hadoop-confext/conf/etc/'"$CLUSTER_NAME"'/hadoop.'"$CLUSTER_NAME"'/hdfs-site.xml"
-c.HDFSBrowserConfig.hdfs_site_namenodes_property = "dfs.ha.namenodes.'"$NAMESPACE"'"
-c.HDFSBrowserConfig.hdfs_site_namenodes_port = "50070"' >> $JPY_CONFIG
--- swan-cern/scripts/before-notebook.d/11_htcondor.sh
+++ swan-cern/scripts/before-notebook.d/11_htcondor.sh
@@ -5,12 +5,6 @@
 # The HT Condor environment is configured here.
 
 # HTCondor at CERN integration
-if [[ ! -n $CERN_HTCONDOR ]]
-then
-  _log "Skipping HTCondor configuration";
-  exit 0;
-fi
-
 if [[ $CERN_HTCONDOR ]]
 then
   export CONDOR_CONFIG=/eos/project/l/lxbatch/public/config-condor-swan/condor_config
@@ -21,7 +15,7 @@ then
 
   # Create self-signed certificate for Dask processes
   _log "Generating certificate for Dask"
-  chown -R $NB_USER:$NB_USER $DASK_TLS_DIR
+  chown -R $NB_USER:$NB_GID $DASK_TLS_DIR
   sudo -u $NB_USER sh /srv/singleuser/create_dask_certs.sh $DASK_TLS_DIR &
 
   # Dask config: lab extension must use SwanHTCondorCluster
@@ -35,4 +29,6 @@ labextension:
     args: []
     kwargs: {}  
   " > $DASK_CONFIG_DIR/labextension.yaml
+else
+   _log "Skipping HTCondor configuration";
 fi

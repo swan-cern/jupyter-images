5d4d93a4c325b5bcf008aaee1cd6bf8548d364df
Pedro Maximino <53193337+PMax5@users.noreply.github.com>
2023-12-06 09:53:55 +0100

    swan-cern: Remove hdfsbrowser related configuration and use jq to format json configuration

--- swan-cern/scripts/before-notebook.d/10_spark.sh
+++ swan-cern/scripts/before-notebook.d/10_spark.sh
@@ -16,38 +16,25 @@ then
   # we don't want to persist the Spark extensions across sessions)
   mkdir -p /etc/jupyter/nbconfig
   _log "Globally enabling the Spark extensions"
-  echo "{
-    \"load_extensions\": {
-      \"sparkconnector/extension\": true,
-      \"hdfsbrowser/extension\": true
+  echo '{
+    "load_extensions": {
+      "sparkconnector/extension": true
     }
-  }" > /etc/jupyter/nbconfig/notebook.json
-  echo "{
-    \"NotebookApp\": {
-      \"jpserver_extensions\": {
-        \"hdfsbrowser.serverextension\": true
+  }' | jq '.' > /etc/jupyter/nbconfig/notebook.json
+
+  echo '{
+    "NotebookApp": {
+      "jpserver_extensions": {
+        "hdfsbrowser.serverextension": true
       }
     }
-  }" > /etc/jupyter/jupyter_notebook_config.json
-  if [ $SPARK_CLUSTER_NAME = "k8s" ]
-  then
-    NAMESPACE="analytix"
-    CLUSTER_NAME="analytix"
-  else
-    NAMESPACE=$(cat /cvmfs/sft.cern.ch/lcg/etc/hadoop-confext/conf/etc/$SPARK_CLUSTER_NAME/$SPARK_CLUSTER_NAME.info.json | python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["namespace"])')
-    CLUSTER_NAME=$SPARK_CLUSTER_NAME
-  fi
-
-  echo 'c.HDFSBrowserConfig.hdfs_site_path = "/cvmfs/sft.cern.ch/lcg/etc/hadoop-confext/conf/etc/'"$CLUSTER_NAME"'/hadoop.'"$CLUSTER_NAME"'/hdfs-site.xml"
-  c.HDFSBrowserConfig.hdfs_site_namenodes_property = "dfs.ha.namenodes.'"$NAMESPACE"'"
-  c.HDFSBrowserConfig.hdfs_site_namenodes_port = "50070"' >> $JPY_CONFIG
+  }' | jq '.' > /etc/jupyter/jupyter_notebook_config.json
 else
   # Disable spark jupyterlab extensions enabled by default if no cluster is selected
   mkdir -p /etc/jupyter/labconfig
   echo "{
     \"disabledExtensions\": {
       \"sparkconnector\": true,
-      \"@swan-cern/hdfsbrowser\": true,
       \"jupyterlab_sparkmonitor\": true
     }
   }" > /etc/jupyter/labconfig/page_config.json

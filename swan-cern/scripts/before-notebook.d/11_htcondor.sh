1c5a6dde6c087368957d1419d98423c591e5a702
Pedro Maximino <53193337+PMax5@users.noreply.github.com>
2023-12-06 14:30:56 +0100

    swan-cern: Grant user permissions to access /srv/dask in the Dockerfile
    Move the `chown` instruction that granted access to the user to the
    directory with the dask certificates to the Dockerfile, as it can be
    executed in build time.

--- swan-cern/scripts/before-notebook.d/11_htcondor.sh
+++ swan-cern/scripts/before-notebook.d/11_htcondor.sh
@@ -8,6 +8,8 @@
 # HTCondor at CERN integration
 if [[ $CERN_HTCONDOR ]]
 then
+  _log "Configuring HTCondor";
+  
   export CONDOR_CONFIG=/eos/project/l/lxbatch/public/config-condor-swan/condor_config
   mkdir -p /etc/condor/config.d/ /etc/myschedd/
   ln -s /eos/project/l/lxbatch/public/config-condor-swan/config.d/10_cernsubmit.erb /etc/condor/config.d/10_cernsubmit.erb
@@ -16,9 +18,7 @@ then
 
   # Create self-signed certificate for Dask processes
   _log "Generating certificate for Dask"
-  chown -R $NB_USER:$NB_GID $DASK_TLS_DIR
   sudo -u $NB_USER sh /srv/singleuser/create_dask_certs.sh $DASK_TLS_DIR &
-  _log "Configuring HTCondor";
 else
    _log "Skipping HTCondor configuration";
 fi

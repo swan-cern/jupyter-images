f2b3ae7f7ce7c5fb31484b9115f57a7318a79743
RodrigoSobral2000 <rodrigosobral@sapo.pt>
2024-02-15 14:11:51 +0100

    First stable version
    - PATH bug fixed (injected in userconfig.sh)
    - PYTHONPATH being unset
    - internal ipykernel installed

--- swan-cern/scripts/others/makenv.sh
+++ swan-cern/scripts/others/makenv.sh
@@ -16,7 +16,7 @@ _log () {
 NAME_ENV=$1
 REQ_PATH=$2
 
-# Help page
+# Help page (passing -h as first argument)
 if [ "$NAME_ENV" = "-h" ]; then
     _log "This script builds a virtual environment."
     _log "Usage: makenv <virtualenv_name> <requirements_file>"
@@ -28,44 +28,50 @@ fi
 # Checks if a name for the environment is given
 if [ -z "$NAME_ENV" ]; then
     _log "ERROR: No virtual environment name provided."
-    exit 0
+    exit 1
 fi
 
 # Checks if a requirements file is given
 if [ -z "$REQ_PATH" ]; then
     _log "ERROR: No requirements file provided."
-    exit 0
+    exit 1
 fi
 
 # Checks if the requirements file is found
 if [ ! -f "$REQ_PATH" ]; then
     _log "ERROR: Requirements file not found."
-    exit 0
+    exit 1
 fi
 
 # Checks if an environment with the same name was already created
 if [ -d "/home/$USER/${NAME_ENV}" ]; then
-    _log "ERROR: Virtual environment already exists. Skipping..."
-    exit 0
+    _log "ERROR: Virtual environment already exists."
+    exit 1
 fi
 
 # Create virtual environment using Python venv
-_log "WARNING: Creating ${NAME_ENV} virtual environment..."
-
-python3 -m venv /home/$USER/${NAME_ENV}
+_log "Creating ${NAME_ENV} virtual environment..."
+python3 -m venv /home/$USER/${NAME_ENV} --copies
 
 # Activate the created virtual environment
-_log "WARNING: Activating virtual environment..."
-
+_log "Activating virtual environment..."
 source /home/$USER/${NAME_ENV}/bin/activate
 
-# Install kernel (within the environment), so it can be ran in Jupyter 
-/home/$USER/${NAME_ENV}/bin/python3 -m ipykernel install --name ${NAME_ENV} --display-name "Python (${NAME_ENV})" --prefix /home/$USER/.local
+# Unset PYTHONPATH to avoid that the virtual environment uses the system packages
+unset PYTHONPATH
 
-# Configure venv kernel so Jupyter can recognise it as a proper Python language
-/home/$USER/${NAME_ENV}/bin/python3 -I /srv/singleuser/configure_kernels_and_terminal.py
+# Ensure pip is installed on venv so ipykernel and requirements can be installed
+python3 -m ensurepip --upgrade
+
+# Install ipykernel so the kernel can be used in Jupyter
+pip3 install ipykernel -q 
+
+# Install kernel (within the environment), so it can be ran in Jupyter 
+python3 -m ipykernel install --name ${NAME_ENV} --display-name "Python (${NAME_ENV})" --prefix /home/$USER/.local
 
 # Install the given requirements
-_log "WARNING: Installing packages from ${REQ_PATH}..."
+_log "Installing packages from ${REQ_PATH}..."
+pip3 install -q -r ${REQ_PATH}
 
-/home/$USER/${NAME_ENV}/bin/python3 -m pip install -r ${REQ_PATH}
+_log "Virtual environment ${NAME_ENV} created successfully."
+_log "WARNING: You may need to refresh the page"

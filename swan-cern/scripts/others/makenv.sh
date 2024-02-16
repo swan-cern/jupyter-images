c241a6447d559a636e31d064348b663376d4a46e
RodrigoSobral2000 <rodrigosobral@sapo.pt>
2024-02-16 16:46:11 +0100

    Environment is now created within an isoleted bash environment (no env vars)
    - building isolation
    - better arguments parsing
    - -c flag added for recreating the env (clears the existing one, if it exists)

--- swan-cern/scripts/others/makenv.sh
+++ swan-cern/scripts/others/makenv.sh
@@ -13,17 +13,41 @@ _log () {
 # This script gets two arguments:
 # $1 is the name of the virtualenv (or -h in case of help page)
 # $2 is the path to the requirements file to install in the virtualenv
-NAME_ENV=$1
-REQ_PATH=$2
+NAME_ENV=
+REQ_PATH=
+CLEAR_ENV=
 
-# Help page (passing -h as first argument)
-if [ "$NAME_ENV" = "-h" ]; then
-    _log "This script builds a virtual environment."
-    _log "Usage: makenv <virtualenv_name> <requirements_file>"
-    _log "Options:"
-    _log "  -h: Display this help message"
-    exit 1
-fi
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h)
+            # Help page (passing -h as first argument)
+            _log "
+This script builds a virtual environment."
+            _log "Usage: makenv <virtualenv_name> <requirements_file>"
+            _log "Options:"
+            _log "  -h: Display this help message"
+            _log "  -c: Clear the virtual environment, if it exists (TBD)
+            "
+            exit 1
+            ;;
+        -c)
+            # If -c is found, shift to next argument and store its value
+            CLEAR_ENV="--clear"
+            shift
+            ;;
+        *)
+            case "requirements.txt" in
+                *$1*)
+                    REQ_PATH=$1
+                    shift
+                    ;;
+                *)
+                    NAME_ENV=$1
+                    shift
+                    ;;
+            esac
+    esac
+done
 
 # Checks if a name for the environment is given
 if [ -z "$NAME_ENV" ]; then
@@ -43,35 +67,60 @@ if [ ! -f "$REQ_PATH" ]; then
     exit 1
 fi
 
-# Checks if an environment with the same name was already created
-if [ -d "/home/$USER/${NAME_ENV}" ]; then
+# Checks if an environment with the same name was already created, if -c is not passed
+if [ -d "/home/$USER/${NAME_ENV}" ] && [ -z "$CLEAR_ENV" ]; then
     _log "ERROR: Virtual environment already exists."
     exit 1
 fi
 
+PATH=$PATH
+USER=$USER
+OAUTH2_FILE=$OAUTH2_FILE
+OAUTH2_TOKEN=$OAUTH2_TOKEN
+KRB5CCNAME=$KRB5CCNAME
+KRB5CCNAME_NB_TERM=$KRB5CCNAME_NB_TERM
+JUPYTER_DOCKER_STACKS_QUIET=$JUPYTER_DOCKER_STACKS_QUIET
+
+# Create a new bash session to avoid conflicts with the current environment in the background
+env -i bash --noprofile --norc << EOF
+
+export PATH=${PATH}
+export USER=${USER}
+export OAUTH2_FILE=${OAUTH2_FILE}
+export OAUTH2_TOKEN=${OAUTH2_TOKEN}
+export KRB5CCNAME=${KRB5CCNAME}
+export KRB5CCNAME_NB_TERM=${KRB5CCNAME_NB_TERM}
+
 # Create virtual environment using Python venv
-_log "Creating ${NAME_ENV} virtual environment..."
-python3 -m venv /home/$USER/${NAME_ENV} --copies
+if [ -z "$CLEAR_ENV" ]; then
+    echo "Creating ${NAME_ENV} virtual environment..."
+else
+    echo "Recreating (-c) ${NAME_ENV} virtual environment..."
+fi
+python3 -m venv /home/$USER/${NAME_ENV} --copies ${CLEAR_ENV}
 
 # Activate the created virtual environment
-_log "Activating virtual environment..."
+echo "Activating virtual environment..."
 source /home/$USER/${NAME_ENV}/bin/activate
 
-# Unset PYTHONPATH to avoid that the virtual environment uses the system packages
-unset PYTHONPATH
-
 # Ensure pip is installed on venv so ipykernel and requirements can be installed
-python3 -m ensurepip --upgrade
+# python3 -m ensurepip --upgrade
 
 # Install ipykernel so the kernel can be used in Jupyter
-pip3 install ipykernel -q 
+python3 -m pip install --upgrade -q pip
+pip install ipykernel -q
 
 # Install kernel (within the environment), so it can be ran in Jupyter 
 python3 -m ipykernel install --name ${NAME_ENV} --display-name "Python (${NAME_ENV})" --prefix /home/$USER/.local
 
 # Install the given requirements
-_log "Installing packages from ${REQ_PATH}..."
-pip3 install -q -r ${REQ_PATH}
+echo "Installing packages from ${REQ_PATH}..."
+pip install -q -r ${REQ_PATH}
 
-_log "Virtual environment ${NAME_ENV} created successfully."
-_log "WARNING: You may need to refresh the page"
+# Copy the requirements file to the virtual environment
+cp ${REQ_PATH} /home/$USER/${NAME_ENV}
+
+echo "Virtual environment ${NAME_ENV} created successfully."
+echo "WARNING: You may need to refresh the page to see the new kernel in Jupyter."
+ 
+EOF

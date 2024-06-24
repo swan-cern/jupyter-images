751ae68432df7f02e2f6dc7bd35c43609bf9f7bf
Pedro Maximino <pedro.maximino@cern.ch>
2024-06-24 17:07:43 +0200

    swan: Add option for users to choose an UI
    Allow users to choose between the classic UI or the new JupyterLab
    interface. The default value choice is the classic UI.

--- swan/python/jupyter_server_config.py
+++ swan/python/jupyter_server_config.py
@@ -18,3 +18,10 @@ c.SwanOauthRenew.files = [
     ("/tmp/cernbox_oauth.token", f"exchanged_tokens/{cernbox_oauth_id}", "{token}"),
     (oauth2_file, f"exchanged_tokens/{eos_oauth_id}", "oauth2:{token}:" + oauth_inspection_endpoint)
 ]
+
+# Toggle JupyterLab based on user preference
+use_jupyterlab = os.environ.get('SWAN_USE_JUPYTERLAB', 'false').lower() == 'true'
+if use_jupyterlab:
+    c.ServerApp.default_url = '/lab'
+else:
+    c.ServerApp.default_url = '/projects'
